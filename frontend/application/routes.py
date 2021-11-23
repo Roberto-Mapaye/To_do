70182805ba813446db59eac85fd0bd72c27614fc
Ubuntu <azureuser@development-server.51upiemndfqurllsdouuqdptxd.zx.internal.cloudapp.net>
2021-11-23 16:03:01 +0000

    New Task

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -1,50 +1,33 @@
-from application import app, db
-from application.models import Tasks
+from application import app
 from application.forms import TaskForm
 from flask import render_template, request, redirect, url_for, jsonify
-from os import getenv
+import requests
 
 @app.route('/')
 @app.route('/home')
 def home():
-    all_tasks = Tasks.query.all()
-    return render_template('index.html', title="Home", all_tasks=all_tasks)
+    all_task = requests.get("http://todo-app-backend:5000/read/allTasks").json()
+    app.logger.info(f"Tasks: {all_task}")
+    return render_template('index.html', title="Home", all_tasks=all_task["tasks"])
 
 @app.route('/create/task', methods=['GET','POST'])
 def create_task():
     form = TaskForm()
 
     if request.method == "POST":
-        new_task = Tasks(description=form.description.data)
-        db.session.add(new_task)
-        db.session.commit()
+        response = requests.post("http://todo-app-backend:5000/create/task", json={"description": form.description.data})
         return redirect(url_for('home'))
 
     return render_template("create_task.html", title="Add a new Task", form=form)
 
-@app.route('/read/allTasks')
-def read_tasks():
-    all_tasks = Tasks.query.all()
-    tasks_dict = {"tasks": []}
-    for task in all_tasks:
-        tasks_dict["tasks"].append(
-            {
-                "description": task.description,
-                "completed": task.completed
-            }
-        )
-    return jsonify(tasks_dict)
-
 @app.route('/update/task/<int:id>', methods=['GET','POST'])
 def update_task(id):
     form = TaskForm()
-    task = Tasks.query.get(id)
+    task = requests.get(f"http://todo-app-backend:5000/read/task/{id}").json()
 
     if request.method == "POST":
-        task.description = form.description.data
-        db.session.commit()
+        response = requests.put(f"http://todo-app-backend:5000/update/task/{id}", json={"description": form.description.data})
         return redirect(url_for('home'))
-
     return render_template('update_task.html', task=task, form=form)
 
 @app.route('/delete/task/<int:id>')
