e3dd6ee82e8937f247d942abcde56b07a18663f8
Ubuntu <rmapaye@dev-jenkins.23nzo2qnrkburp5xzshz5xehbd.parx.internal.cloudapp.net>
2021-11-26 13:44:06 +0000

    Added stuff

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -4,13 +4,12 @@ echo "Setup stage"
 
 # installing requirements
 sudo apt-get update
-sudo apt install curl -y
+sudo apt install -y curl jq
 curl https://get.docker.com | sudo bash
 
 sudo usermod -aG docker jenkins
 newgrp docker
-sudo apt update
-sudo apt install -y curl jq
+
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
